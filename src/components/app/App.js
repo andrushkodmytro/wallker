bb98cd1935f9e892a1c0836916ea7c1cf513206e
Dmytro Andrushko <andrushkodima@gmail.com>
2018-08-31 15:46:09 +0300

    Calendar fix after ferge

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,6 +4,7 @@ import LandingPage from "../landing/LandingPage";
 import SignIn from "../signin/signinPage/SignInPage";
 import {BrowserRouter as Router, Route,Switch} from "react-router-dom";
 import PrivateRoutDashboard from "../privateRoutDashboard/PrivateRoutDashboard";
+import Dashboard from "../dashboard/Dashboard";
 import NotFound from "../notFound/NotFound";
  
 class App extends Component {
@@ -17,7 +18,8 @@ class App extends Component {
             <Route path="/signin" component={SignIn}/>
             <Route path="/forgot" component={SignIn}/>
             <Route path="/signup" component={SignIn}/>
-            <Route path="/dashboard" component={PrivateRoutDashboard}/>
+            {/* <Route path="/dashboard" component={PrivateRoutDashboard}/> */}
+            <Route path="/dashboard" component={Dashboard}/>
             <Route component={NotFound}/>
           </Switch>
           
