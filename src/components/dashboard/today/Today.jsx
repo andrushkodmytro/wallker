af9e3c23e49150a4ce90aa90e3a6ce1c8f0346ae
Dmytro Andrushko <andrushkodima@gmail.com>
2018-09-18 11:58:29 +0300

    test

--- src/components/dashboard/today/Today.jsx
+++ src/components/dashboard/today/Today.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Iframe from './Iframe.jsx';
+import Map from "../map/Map";
 import TodayProfile from './TodayProfile.jsx';
 import TodayResults from './TodayResults.jsx';
 import './Today.css';
@@ -13,13 +14,14 @@ export default class Today extends Component {
         <div className="today">
             <TodayProfile user={ user }/> 
             <TodayResults user={ user }/>
-            <Iframe 
+            {/* <Iframe 
               title="GoogleMaps" 
               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10295.141676931713!2d24.053729794851343!3d49.82769810840492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1suk!2sus!4v1533239889011"
               width="100%" 
               height="248" 
               allowFullScreen="allowfullscreen" 
-              frameBorder="0"/>
+              frameBorder="0"/> */}
+              <Map/>
         </div>
       );
     }
