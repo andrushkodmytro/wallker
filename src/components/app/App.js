79173610adbf32dd3365b34dcdc8e49aeefb4ec0
dima <andrushkodima@gmail.com>
2023-09-05 14:32:50 +0300

    Add git hub pages config

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   render() {
 
     return (
-      <Router>
+      <Router basename='/wallker/'>
         <div>
           <Switch>
             <Route exact path="/" component={LandingPage}/>
